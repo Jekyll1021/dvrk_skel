97d6d8347c9c16fadc21f01483bb0a327a935cfe
Zisu Dong <j_dong1021@berkeley.edu>
2017-04-27 18:28:20 -0700

    add translation

--- ImageSubscriber.py
+++ ImageSubscriber.py
@@ -80,9 +80,57 @@ def contour_detector(image, rgb_range):
     opening = cv2.morphologyEx(rest, cv2.MORPH_OPEN, kernel)
     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
     grey = cv2.cvtColor(closing, cv2.COLOR_BGR2GRAY)
-    rest, contours, hierarchy = cv2.findContours(grey, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    thr = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY)[1]
+    rest, contours, hierarchy = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    ######################## latest addition
+    lst = []
+    for contour in contours:
+        epsilon = 0.05 * cv2.arcLength(contour, True)
+        moment = cv2.moments(contour)
+        cx = moment["m10"] / moment['m00']
+        cy = moment['m01'] / moment['m00']
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        lst.append([contour, np.array([cx, cy])])
 
-    return contours
+    #################################
+    # return contours
+    return lst
+
+# left to be tested
+def find_right_to_left_transformation(left_image, right_image, rgb_range):
+    """find corresponding contours between the two images"""
+    left_contours = contour_detector(left_image, rgb_range)
+    right_contours = contour_detector(right_image, rgb_range)
+    # sort by bounded area
+    correspondence_lst = []
+    left_clst = []
+    right_clst = []
+    ## find correspondence
+    for left_contour in left_contours:
+        for right_contour in right_contours:
+            if left_contour[1][0] - right_contour[1][0] < 90 and left_contour[1][0] - right_contour[1][0] > 50 and abs(left_contour[1][1] - right_contour[1][1]) < 10:
+                correspondence_lst.append([left_contour[0], right_contour[0]])
+                left_clst.append(left_contour[1])
+                right_clst.append(right_contour[1])
+                break
+    left_clst = np.array(left_clst)
+    right_clst = np.array(right_clst)
+    # # find transformation
+    # mapping_mat = left_clst.T.dot(right_clst)
+    # find translation vector
+    return np.mean(left_clst - right_clst), left_clst, right_clst
+    # for l in left_contours:
+    #     left_clst.append(l[1])
+    # for r in right_contours:
+    #     right_clst.append(r[1])
+    # left_clst = np.asarray(left_clst)
+    # right_clst = np.asarray(right_clst)
+    # return left_clst, right_clst
+
+
+
+
 
 
 
